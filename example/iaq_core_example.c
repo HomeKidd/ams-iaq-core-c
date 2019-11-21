ebed9e92c7f101c2873447c0b8f7c889a0e75f67
HomeKid <homekid92@gmail.com>
2019-11-21 09:20:02 +0100

    Update iaq_core_example.c

--- example/iaq_core_example.c
+++ example/iaq_core_example.c
@@ -9,10 +9,10 @@
 #include "i2c/i2c.h"
 #include "iaq_core.h"
 
-#define SENSOR_POLL_PERIOD 10000  // reading time
+#define SENSOR_POLL_PERIOD 10000  // reading interval
 
-#define SCL_PIN 5
-#define SDA_PIN 4
+#define I2C_SCL_PIN 5
+#define I2C_SDA_PIN 4
 #define I2C_BUS 0
 
 static iaq_core_sensor_t* iaq_core; // iAQ Core device data structure
@@ -24,16 +24,11 @@ uint16_t co2_value;
 
 while(1){
 
-
         while (iaq_core_is_heating(iaq_core)){  // busy waiting until sensor is heating up
-                printf("Sensor is heating up...\n It takes about 5 minutes.\n");
-                vTaskDelay(5000 / portTICK_PERIOD_MS);
+                printf("!!! WARNING !!! Sensor is in Heating up phase which takes about 5 minutes from startup.\n");
+                vTaskDelay(SENSOR_POLL_PERIOD / portTICK_PERIOD_MS);
                 }
 
-        if (!iaq_core_get_results(iaq_core, &tvoc_value, &co2_value)){
-                printf("iAQ Core C reading failed\n");
-                vTaskDelay(5000 / portTICK_PERIOD_MS);
-            }
         while (iaq_core_is_measuring(iaq_core)){}; // busy waiting until measurement results are available
 
         if (iaq_core_get_results(iaq_core, &tvoc_value, &co2_value)){
@@ -41,18 +36,18 @@ while(1){
             }
 
         else {
-
+                printf("!!! ERROR !!! iAQ Core C reading failed\n");
                 // error happened
             switch (iaq_core->error_code)
                 {
                     case IAQ_CORE_I2C_BUSY:
-                                            printf("Sensor is BUSY...\n");
+                                            printf("!!! ERROR !!! Sensor is BUSY...\n");
                                             break;
                     case IAQ_CORE_I2C_READ_FAILED:
-                                            printf("I2C BUS read failed...\n");
+                                            printf("!!! ERROR !!! I2C BUS read failed...\n");
                                             break;                                            
                     case IAQ_CORE_SENSOR_ERROR:
-                                            printf("Sensor error, its might be broken...\n");
+                                            printf("!!! ERROR !!! Sensor error, its might be broken...\n");
                                             break;                    
                     default:
                             printf(" ");
@@ -72,5 +67,5 @@ void user_init(void)
     i2c_set_clock_stretch (I2C_BUS, IAQCORE_I2C_CLOCK_STRETCH);
     iaq_core = iaq_core_init_sensor (I2C_BUS, IAQCORE_I2C_ADDR);
 
-    if (iaq_core) xTaskCreate(iaq_core_task, "iaq_core_task", 256, NULL, 2, NULL);
+    if (iaq_core) xTaskCreate(iaq_core_task, "iaq_core_task", 256, NULL, 2, NULL); // If iAQ Core is available start the user_task
 }
